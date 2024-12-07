39847a1bfb6b88d1a7e4f028711ab7375a870453
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2024-12-08 02:59:07 +0530

    Update AuthController.java

--- src/main/java/com/studentworkshop/controllers/AuthController.java
+++ src/main/java/com/studentworkshop/controllers/AuthController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000","https://online-workshop-nine.vercel.app")
 public class AuthController {
 
     @Autowired
