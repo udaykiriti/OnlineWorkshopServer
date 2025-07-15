430b80dbb0a750a389fc3f4676a8cada394232be
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-07-15 08:08:06 +0530

    Update StudentWorkshop.java

--- src/main/java/com/studentworkshop/StudentWorkshop.java
+++ src/main/java/com/studentworkshop/StudentWorkshop.java
@@ -1,13 +1,10 @@
 package com.studentworkshop;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentWorkshop {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentWorkshop.class, args);
 	}
-
 }
