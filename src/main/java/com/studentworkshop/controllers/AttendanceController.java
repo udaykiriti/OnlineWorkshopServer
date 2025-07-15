5a780e69f22fc7f10ba82e83b2ac50bbaf47dceb
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-07-15 08:04:40 +0530

    Update AttendanceController.java

--- src/main/java/com/studentworkshop/controllers/AttendanceController.java
+++ src/main/java/com/studentworkshop/controllers/AttendanceController.java
@@ -1,15 +1,12 @@
 package com.studentworkshop.controllers;
-
 import com.studentworkshop.models.Attendance;
 import com.studentworkshop.models.Registration;
 import com.studentworkshop.services.AttendanceService;
 import com.studentworkshop.services.RegistrationService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
