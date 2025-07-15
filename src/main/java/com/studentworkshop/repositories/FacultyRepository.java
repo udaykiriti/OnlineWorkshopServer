7515046d8d514b48d0bc2a16e065359687775501
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-07-15 08:06:29 +0530

    Update FacultyRepository.java

--- src/main/java/com/studentworkshop/repositories/FacultyRepository.java
+++ src/main/java/com/studentworkshop/repositories/FacultyRepository.java
@@ -1,8 +1,6 @@
 package com.studentworkshop.repositories;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.studentworkshop.models.Faculty;
 
 @Repository
