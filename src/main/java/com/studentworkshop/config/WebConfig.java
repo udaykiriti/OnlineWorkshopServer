cca239b7e9d88225bbd09978e3345b771e015628
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-08-25 13:58:07 +0530

    Enhance CORS configuration comments in WebConfig

--- src/main/java/com/studentworkshop/config/WebConfig.java
+++ src/main/java/com/studentworkshop/config/WebConfig.java
@@ -8,14 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    // Define a Bean that customizes the WebMvcConfigurer for CORS configuration
     @Bean
     WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+            // Override the addCorsMappings method to configure CORS settings
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                // Allow CORS requests to any endpoint that matches /api/**
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000","https://online-workshop-nine.vercel.app")
+                        // Specify the allowed origins for cross-origin requests
+                        .allowedOrigins("http://localhost:3000", "https://online-workshop-nine.vercel.app")
+                        // Define the HTTP methods that are allowed for CORS requests
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        // Allow all headers in the request
                         .allowedHeaders("*");
             }
         };
